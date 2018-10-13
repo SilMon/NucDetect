e093f9502c48486d6edf5eebe61d6459e3cc3e0d
SilMon <weiss-romano@web.de>
2018-10-13 11:32:33 +0200

    Update ROI_Handler.py

--- Nucleus/image/ROI_Handler.py
+++ Nucleus/image/ROI_Handler.py
@@ -2,6 +2,7 @@
 Created on 06.10.2018
 
 '''
+from skimage.draw import ellipse_perimeter
 from Nucleus.image.ROI import ROI
 from Nucleus.image import channel
 
@@ -13,7 +14,12 @@ class ROI_Handler:
 
     def __init__(self,blue_ws, green_ws, red_ws):
         '''
-        Constructor
+        Constructor of the class.
+        
+        Keyword arguments:
+        blue_ws(2D numpy): Blue channel watershed 
+        green_ws(2D numpy): Green channel watershed
+        red_ws (2D numpy): Red channel watershed
         '''
         self.blue_ws = blue_ws
         self.red_ws = red_ws
@@ -23,6 +29,9 @@ class ROI_Handler:
         self.red = [None] * 500
             
     def analyse_image(self):
+        '''
+        Method to analyse an image according to the given data
+        '''
         #Analysis of the blue channel
         for y in range(len(self.blue_ws)):
             for x in range(len(self.blue_ws[0])):
@@ -70,3 +79,62 @@ class ROI_Handler:
                             red_rem.append(red)
             self.green = [x for x in self.green if x not in gre_rem]
             self.red = [x for x in self.red if x not in red_rem]
+     
+    def draw_roi(self, img_array):
+        '''
+        Method to draw the ROI saved in this handler on the image
+        
+        Keyword arguments:
+        img_array(ndarray): The image to draw the ROI on.
+        
+        Returns:
+        ndarray -- The image with the drawn ROI
+        '''
+        canvas = img_array.copy()
+        for roi in self.nuclei:
+            if roi is not None:
+                self._draw_roi(canvas, roi, (50,50,255))
+                for green in roi.green:
+                    if green is not None:
+                        self._draw_roi(canvas, green, (50,255,50))
+                for red in roi.red:
+                    if red is not None:
+                        self._draw_roi(canvas, red, (255,50,50))
+        return canvas
+    
+    def _draw_roi(self, img_array, roi, col):
+        '''
+        Private method to draw rois on a image. 
+        
+        Keyword arguments:
+        img_array(ndarray): The image to draw the ROI on.
+        roi(ROI): The roi to draw on the image
+        col(3D tuple): The color in which the roi should be highlighted 
+        '''
+        data = roi.get_data()
+        rr, cc = ellipse_perimeter(data.get("center")[1], data.get("center")[0], data.get("height")//2, data.get("width")//2, shape=img_array.shape)
+        img_array[rr,cc,:]= col
+         
+            
+    def get_data(self,console=True):
+        '''
+        Method to obtain the data stored in this handler
+        
+        Keyword arguments:
+        console(bool): Determines if the obtained results are formatted and printed to the console
+        
+        Returns: 
+        str -- The data as .csv string
+        '''
+        if console:
+            heading = "{0:^15};{1:^15};{2:^15};{3:^15};{4:^15};{5:^15}".format("Index","Width","Height","Center","Green Foci","Red Foci")
+            print(heading)
+            ind = 0
+            for roi in self.nuclei:
+                if roi != None:
+                    data = roi.get_data()
+                    print("{0:^15};{1:^15};{2:^15};{3:^15};{4:^15};{5:^15}".format(ind,data.get("width"),data.get("height"),str(data.get("center")),len(data.get("green roi")),len(data.get("red roi"))))
+                    ind += 1
+        else:
+            pass#TODO
+    
