a93ac2097c5b77b46186378bcdbd25476b78f094
SilMon <weiss-romano@web.de>
2018-10-15 17:07:10 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+'''
+Created on 02.10.2018
+
+@author: Romano Weiss
+'''
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="Nuclear Detector",
+    version="0.0.1-1",
+    description=("Module to identify intranuclear proteins on basis of"
+                 "fluorescence images."),
+    author="Romano Weiss",
+    url="https://github.com/SilMon/NucDetect",
+    long_description=read("README"),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=find_packages(),
+)
